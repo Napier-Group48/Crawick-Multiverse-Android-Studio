a7b7b2bfc515b137afbbf16c878e9268126b1031
Ross Houliston <ross.houliston1@gmail.com>
2018-03-28 19:16:02 +0100

    First Push of the app.
    - Basic template created including Hamburger menu and navigation bar, with several "fragment" pages created to be filled in. Splash Screen included.

---
+++ CrawickMultiverse3/app/src/main/java/com/example/work/crawickmultiverse/ThirdBurgerFragment.java
@@ -0,0 +1,29 @@
+package com.example.work.crawickmultiverse;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ThirdBurgerFragment extends Fragment {
+
+
+    public ThirdBurgerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_third_burger, container, false);
+    }
+
+}
