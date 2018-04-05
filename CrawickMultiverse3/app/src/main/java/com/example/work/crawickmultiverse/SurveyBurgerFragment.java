52ef94fe993450f6bd1a3699cd7144089a03e02a
KiwiMC1482 <30599176+KiwiMC1482@users.noreply.github.com>
2018-04-05 18:43:32 +0100

    Survey page
    Survey page currently has 1 textview, edittext and button. Also contains a toast to pop up when button is pressed

--- CrawickMultiverse3/app/src/main/java/com/example/work/crawickmultiverse/SurveyBurgerFragment.java
+++ CrawickMultiverse3/app/src/main/java/com/example/work/crawickmultiverse/SurveyBurgerFragment.java
@@ -1,29 +1,49 @@
 package com.example.work.crawickmultiverse;
 
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SurveyBurgerFragment extends Fragment {
+public class SurveyBurgerFragment extends Fragment
+{
+    Button submit_button;
 
-
-    public SurveyBurgerFragment() {
+    public SurveyBurgerFragment()
+    {
         // Required empty public constructor
+
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second_burger2, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_fourth_burger, container, false);
+
+        Button button = (Button) v.findViewById(R.id.submit_button);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // do something
+                Toast.makeText(getActivity(),"Message", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return v;
+
     }
 
 }
