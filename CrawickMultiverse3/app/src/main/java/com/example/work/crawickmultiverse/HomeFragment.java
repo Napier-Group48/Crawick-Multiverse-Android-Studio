73c46952d2c7b96adfaf2f869124070b73566c1c
KiwiMC1482 <30599176+KiwiMC1482@users.noreply.github.com>
2018-04-05 21:58:08 +0100

    Picture on exhibits page
    Picture shows on exhibits page

--- CrawickMultiverse3/app/src/main/java/com/example/work/crawickmultiverse/HomeFragment.java
+++ CrawickMultiverse3/app/src/main/java/com/example/work/crawickmultiverse/HomeFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 /**
@@ -22,9 +23,11 @@ public class HomeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
-        myView = inflater.inflate(R.layout.fragment_home, container, false);
-        return myView;
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return v;
     }
 }
