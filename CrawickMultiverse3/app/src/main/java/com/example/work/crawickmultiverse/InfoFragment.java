6326a7594344a9a64e6feca6adb73ce9b80674f7
KiwiMC1482 <30599176+KiwiMC1482@users.noreply.github.com>
2018-04-05 16:53:28 +0100

    Information page
    Webview to information page

--- CrawickMultiverse3/app/src/main/java/com/example/work/crawickmultiverse/InfoFragment.java
+++ CrawickMultiverse3/app/src/main/java/com/example/work/crawickmultiverse/InfoFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 /**
@@ -25,8 +27,10 @@ public class InfoFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_info, container, false);
 
-
-
+        WebView webView = (WebView)v.findViewById(R.id.wv);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.crawickmultiverse.co.uk/visitor-information/opening-times-and-prices/");
 
         return v;
     }
