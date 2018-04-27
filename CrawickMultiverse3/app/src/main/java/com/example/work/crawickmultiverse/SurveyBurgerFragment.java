9d0bf53415ea2a387a398eb04647dc15c1976d0f
KiwiMC1482 <30599176+KiwiMC1482@users.noreply.github.com>
2018-04-27 01:06:13 +0100

    Survey page content added

--- CrawickMultiverse3/app/src/main/java/com/example/work/crawickmultiverse/SurveyBurgerFragment.java
+++ CrawickMultiverse3/app/src/main/java/com/example/work/crawickmultiverse/SurveyBurgerFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 /**
@@ -20,22 +22,18 @@ public class SurveyBurgerFragment extends Fragment
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-    {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_fourth_burger, container, false);
 
         Button button = v.findViewById(R.id.submit_button);
-        button.setOnClickListener(new View.OnClickListener()
-        {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 // do something
-                Toast.makeText(getActivity(),"Message", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "Survey submitted", Toast.LENGTH_SHORT).show();
             }
         });
-        return v;
+            return v;
+        }
     }
-
-}
